a4c658abe144747b9db9c324b4a2faf306f9fa3e
thakur7071 <singhnaveen2370@gmail.com>
2024-01-21 17:35:04 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function imgSlider(anything) {
+    document.querySelector('.chaiimg').src = anything;
+}
+function changeCircleColor(color) {
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
